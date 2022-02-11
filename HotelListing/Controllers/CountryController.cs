cf86fdad1c3a0707cc18d65ab13f58f493d36d30
nDaniel <canh.chn@gmail.com>
2022-02-11 16:09:40 +0700

    feat: create basic get all countries and get by Id endpoint

---
+++ HotelListing/Controllers/CountryController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using HotelListing.IRepository;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<CountryController> _logger;
+        private readonly IMapper _mapper;
+
+        public CountryController(IUnitOfWork unitOfWork, ILogger<CountryController> logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            try
+            {
+                var countries = await _unitOfWork.Countries.GetAll();
+                var countriesDTO = _mapper.Map<List<CountryDTO>>(countries);
+                return Ok(countriesDTO);
+            } catch (Exception e)
+            {
+                _logger.LogError(e, "Some thing went wrong");
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCountry(int id)
+        {
+            try
+            {
+                var country = await _unitOfWork.Countries.Get(c => c.Id == id, new List<string> { "Hotels" });
+                var countryDTO = _mapper.Map<CountryDTO>(country);
+                return Ok(countryDTO);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Some thing went wrong");
+                return BadRequest();
+            }
+        }
+    }
+}
