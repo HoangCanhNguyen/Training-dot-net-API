c777533a4d66383bf424abaaef61ee2e6fa9306f
nDaniel <canh.chn@gmail.com>
2022-02-14 17:47:05 +0700

    feat: add Identity and working with JWT

---
+++ HotelListing/Migrations/20220214041520_AddedRoleIdentity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class AddedRoleIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "6aeb364b-87a0-4889-813c-a57047b4eed4", "1624cdde-53d8-4497-a951-2bdc6ca5800b", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b43af77c-ac45-4906-a56d-e3b3a9f704fa", "b2eb3aac-7b28-4e02-bae0-3ee48c2bf613", "Admin", "ADMIN" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6aeb364b-87a0-4889-813c-a57047b4eed4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b43af77c-ac45-4906-a56d-e3b3a9f704fa");
+        }
+    }
+}
---
+++ HotelListing/services/AuthManager.cs
@@ -0,0 +1,110 @@
+﻿using HotelListing.Data;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelListing.services
+{
+    public class AuthManager : IAuthManager
+    {
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly IConfiguration _configuration;
+        private ApiUser _user;
+
+        public AuthManager(UserManager<ApiUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+        public async Task<string> CreateToken()
+        {
+            var signingCredentials = GetSigningCredentials();
+            var claims = await GetClaims();
+            var tokenOptions = GenerateTokenOtions(signingCredentials, claims);
+
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+            //IsValidToken(token);
+
+            return token;
+        }
+
+
+        private JwtSecurityToken GenerateTokenOtions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("Jwt");
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings.GetSection("Issuer").Value,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: signingCredentials
+                );
+            return token;
+        }
+
+        public bool IsValidToken(string token)
+        {
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var jwtSettings = _configuration.GetSection("Jwt");
+            try
+            {
+                jwtSecurityTokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.GetSection("Key").Value)),
+
+                    ValidateIssuer = false,
+                    ValidIssuer = jwtSettings.GetSection("Issuer").Value,
+
+                   // ValidateAudience = false,
+                  //  ValidAudience = _options.Audience,
+
+                    ValidateLifetime = false,
+                    ClockSkew = TimeSpan.Zero
+                }, out var _);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, _user.UserName)
+            };
+
+            var roles = await _userManager.GetRolesAsync(_user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = _configuration.GetSection("Jwt").GetSection("Key").Value;
+            var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        public async Task<bool> ValidateUser(LoginDTO userDTO)
+        {
+            _user = await _userManager.FindByNameAsync(userDTO.Email);
+            return (_user != null && await _userManager.CheckPasswordAsync(_user, userDTO.Password));
+        }
+    }
+}
